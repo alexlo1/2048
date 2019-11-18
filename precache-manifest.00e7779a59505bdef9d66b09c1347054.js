7a6738bccc3d89d8014a861b8ef298e9f001b278
alexlo1 <alexdlo8967@gmail.com>
2019-11-18 12:55:40 -0800

    Updates

---
+++ precache-manifest.00e7779a59505bdef9d66b09c1347054.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d561505910a66407e0e6cbf73833546a",
+    "url": "/2048/index.html"
+  },
+  {
+    "revision": "87a2963eb40a4ac62522",
+    "url": "/2048/static/css/main.6e4f5b06.chunk.css"
+  },
+  {
+    "revision": "cfbbe3cb9fc3855b2b95",
+    "url": "/2048/static/js/2.afb6d4a8.chunk.js"
+  },
+  {
+    "revision": "87a2963eb40a4ac62522",
+    "url": "/2048/static/js/main.68157581.chunk.js"
+  },
+  {
+    "revision": "d144adf5bcb8e9ea4d64",
+    "url": "/2048/static/js/runtime-main.06a04d16.js"
+  }
+]);
